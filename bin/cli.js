9f655dd875ba1b38495507af9bcf402f856ea90a
Andrew de Andrade <aandrade@uber.com>
2014-11-18 17:56:34 -0800

    Moar refactoring

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
 'use strict';
-
+var process = require('process');
+var console = require('console');
 var parseArgs = require('minimist');
 
 var runbrowser = require('../index.js');
@@ -13,38 +14,42 @@ var port = Number(args.p || args.port) || 3000;
 var help = args.help || args.h || args._.length === 0;
 var phantom = args.b || args.phantom || args.phantomjs;
 var coverage = args.p || args.coverage || args.istanbul;
+if (coverage) phantom = true;
 
 if (help) {
-  console.log();
-  console.log('Usage:');
-  console.log();
-  console.log('  run-browser test-file.js');
-  console.log();
-  console.log('options:');
-  console.log();
-  console.log('  -p --port <number> The port number to run the server on (default: 3000)');
-  console.log('  -b --phantom       Use the phantom headless browser to run tests and then exit with the correct status code (if tests output TAP)');
-  console.log('');
+  var helpText = [
+    '',
+    'Usage:',
+    '  run-browser <file> <options>',
+    '',
+    'Options:',
+    '  -p --port <number> The port number to run the server on (default: 3000)',
+    '  -b --phantom       Use the phantom headless browser to run tests and then exit with the correct status code (if tests output TAP)',
+    '  -c --coverage      Generate coverage Istanbul report (phantomjs only). Repeat for each type of coverage report desired. (default: text only)',
+    '',
+    'Example:',
+    '  run-browser test-file.js --port 3030 --coverage text --coverage html --coverage=cobertura',
+    ''
+  ].join('\n');
+  console.log(helpText);
   process.exit(process.argv.length === 3 ? 0 : 1);
 }
 
-var server = runbrowser(filename, coverage);
+var server = runbrowser(filename);
 server.listen(port);
 
 if (!phantom) {
   console.log('Open a browser and navigate to "http://localhost:' + port + '"');
 } else {
-  runbrowser.runPhantom('http://localhost:' + port + '/',
-    function (err, res) {
-      if (err) {
-        console.error(err);
-        throw err;
-      }
+  runbrowser.runPhantom('http://localhost:' + port + '/', coverage, function (err, res) {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
 
-      if (res.stderr) process.stderr.write(res.stderr);
-      if (res.stdout) process.stdout.write(res.stdout);
+    if (res.stderr) process.stderr.write(res.stderr);
+    if (res.stdout) process.stdout.write(res.stdout);
 
-      process.exit(res.passed ? 0: 1);
-    });
+    process.exit(res.passed ? 0 : 1);
+  });
 }
-
